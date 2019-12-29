e890bbbc247e4ba7703a792af16930e3c0f5f149
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-12-29 08:28:48 -0800

    - initial commit Web Driver class created and connection established with the Chrome driver. Succesfully navigate to the inital home page.

--- web_automation/Program.cs
+++ web_automation/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
+﻿/*# @author: Raul Rivero Rubio
+### @date:  12/27/2019
+### @version: 1.0
+### @brief: 
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace web_automation
 {
     class Program
     {
+        const string RELATIVE_PATH = "C:\\Users\\Jesus\\Documents\\C#\\alliante_automation\\web_automation\\web_automation\\drivers\\chromedriver_win32";
+        const string HOMEPAGE = "https://www.allegiantair.com";
+
+
         static void Main(string[] args)
         {
+            web_automation.WebDriver driver = new WebDriver(RELATIVE_PATH, IBrowser.CHROME);
+
+            if (driver.CurrentDriver != null)
+            {
+                GoToHomePage(driver);
+            }
+        }
+
+        static void GoToHomePage(web_automation.WebDriver driver)
+        {
+            try {
+                driver.CurrentDriver.Navigate().GoToUrl(HOMEPAGE);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ClosePopUpWindow(web_automation.WebDriver driver)
+        {
+
         }
     }
 }
---
+++ web_automation/WebDriver.cs
@@ -0,0 +1,69 @@
+﻿/*# @author: Raul Rivero Rubio
+### @date:  12/27/2019
+### @version: 1.0
+### @brief: webdriver class that creates a connect with a
+###         specific web browser. If successful it it will
+###         a browser object ready for automation.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace web_automation
+{
+    public enum IBrowser { CHROME, FIREFOX, EDGE}
+
+    public class WebDriver
+    {
+        private IWebDriver webdriver = null;
+
+        public WebDriver(string path, IBrowser browser)
+        {
+            Establish(path, browser);
+        }
+
+        public void Establish(string path, IBrowser browser)
+        {
+            switch(browser)
+            {
+                case IBrowser.CHROME: withChrome(path); break;
+                case IBrowser.FIREFOX: ; break;
+                case IBrowser.EDGE:; break;
+            }
+        }
+
+        private void withChrome(string path)
+        {
+            try
+            {
+                webdriver = new ChromeDriver(path);
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Close()
+        {
+            webdriver.Close();
+            webdriver = null;
+        }
+
+        public bool isConEstablished()
+        {
+            return webdriver != null;
+        }
+
+        public IWebDriver CurrentDriver {
+            get {
+                return webdriver;
+            }
+        }
+    }
+}
