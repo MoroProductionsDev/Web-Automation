55bce5a376786179a7a7cf9acbe117a059501e5f
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-12-31 09:22:55 -0800

    - Book trip mostly completed. - Code needs recycling (refactoring).

--- web_automation/Program.cs
+++ web_automation/Program.cs
@@ -3,15 +3,18 @@
 ### @version: 1.0
 ### @brief: 
 */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
 namespace web_automation
 {
@@ -29,47 +32,182 @@ namespace web_automation
             {
                 GoToHomePage(driver);
                 ClosePopUpWindow(driver);
+                BookTrip(driver);
             }
         }
 
         static void GoToHomePage(web_automation.WebDriver driver)
         {
-            try {
-                driver.CurrentDriver.Navigate().GoToUrl(HOMEPAGE);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            driver.NavigateTo(HOMEPAGE);
         }
 
         static void ClosePopUpWindow(web_automation.WebDriver driver)
         {
-            try
+            IWebElement modalPopup;
+            if ((modalPopup = driver.FindElement("ui-icon-closethick", Element_By.CLASSNAME)) != null)
             {
-                //driver.CurrentDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.CurrentDriver.FindElement(By.ClassName("ui-icon-closethick")).Click();
-                Console.WriteLine("close pop");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                modalPopup.Click();
             }
         }
 
-        /*
+
         static void BookTrip(web_automation.WebDriver driver)
         {
-            try
+            IWebElement form_input;
+            IReadOnlyCollection<IWebElement> elemlist;
+            const string DEPARTURE_CITY = "Las Vegas, NV (LAS)";
+            const string DESTINATION_CITY = "Albuquerque, NM (ABQ)";
+            const int ROUND_TRIP = 0;
+            const int ONEWAY = 1;
+            int choice = 1;
+
+            Thread.Sleep(1000);
+            if ((form_input = driver.FindElement("search_form[departure_city]", Element_By.NAME)) != null)
             {
-                IWebElement input = driver.CurrentDriver.FindElement(By.Name("search_form[departure_city]"));
-                SelectElement selectElement = new SelectElement(input);
+                try
+                {
+                    form_input.Click();
+                    form_input.SendKeys(DEPARTURE_CITY);
+                }
+                catch (ElementClickInterceptedException elmClkInt_Ex)
+                {
+                    Console.WriteLine(elmClkInt_Ex.Message);
+                }
             }
-            catch (Exception ex)
+
+            if ((form_input = driver.FindElement("search_form[destination_city]", Element_By.NAME)) != null)
             {
-                Console.WriteLine(ex.Message);
+                form_input.Click();
+                try
+                {
+                    if (DESTINATION_CITY != DEPARTURE_CITY)
+                    {
+                        form_input.SendKeys(DESTINATION_CITY);
+                    }
+                    else
+                    {
+                        throw new System.ArgumentException($"Departure city and destination city most be different. {DEPARTURE_CITY} : {DESTINATION_CITY}");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            elemlist = driver.FindElements("search_form[trip_type]", Element_By.NAME);
+            Console.WriteLine(elemlist);
+            if (choice == ROUND_TRIP)
+            {
+                driver.FindElement("//input[@type='radio'][@value='return']", Element_By.XPATH).Click();
+
+                // departure date
+                NextAvailablesDate(driver, "search_form[departure_date]", "depart");
+
+                // return date
+                NextAvailablesDate(driver, "search_form[return_date]", "return");
+            }
+            else if (choice == ONEWAY)
+            {
+                driver.Wait(100);
+                driver.FindElement("//input[@type='radio'][@value='return']", Element_By.XPATH).Click();
+                driver.FindElement("//input[@type='radio'][@value='oneway']", Element_By.XPATH).Click();
+
+                // departure date
+                NextAvailablesDate(driver, "search_form[departure_date]", "depart");
             }
         }
-        */
+
+        private static void NextAvailablesDate(web_automation.WebDriver driver, string value, string at)
+        {
+            bool foundAvailableDate = false;
+            string attr;
+            IWebElement datepicker_div = null;
+            IWebElement table_head = null;
+            IWebElement table_body = null;
+            IWebElement form_input = null;
+            IWebElement table = null;
+            IWebElement nextmonth = null;
+            IReadOnlyCollection<IWebElement> rows = null;
+            IReadOnlyCollection<IWebElement> tds = null;
+            WebDriverWait wait = new WebDriverWait(driver.CurrentDriver, TimeSpan.FromSeconds(20));
+
+            Actions actions = new Actions(driver.CurrentDriver);
+            if ((form_input = driver.FindElement(value, Element_By.NAME)) != null)
+            {
+                try
+                {
+                    form_input.Click();
+                } catch (ElementClickInterceptedException elemClckIntEX)
+                {
+                    Console.WriteLine(elemClckIntEX);
+                }
+            }
+
+            Thread.Sleep(1000);
+            form_input.Click();
+            try
+            {
+                // 
+                datepicker_div = driver.FindElement(at, Element_By.CLASSNAME);
+                // actions.DoubleClick(calender).Perform();
+                //form_input.FindElement(By.ClassName("datepicker-toggle")).Click();
+                //table = datepicker_div.FindElement(By.ClassName("table"));
+
+                //table_body = datepicker_div.FindElement(By.TagName("tbody
+
+                //rows = table_body.FindElements(By.TagName("tr"));
+
+                //foreach (IWebElement row in rows)
+                //{
+                datepicker_div = driver.FindElement(at, Element_By.CLASSNAME);
+
+                while (!foundAvailableDate)
+                {
+                    if (at == "depart")
+                    {
+                        table = driver.CurrentDriver.FindElement(By.XPath("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[1]/div/div/div/div[2]/table"));
+                    }
+                    else
+                    {
+                        table = driver.CurrentDriver.FindElement(By.XPath("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/table"));
+                    }
+
+                    //wait.Until(ExpectedConditions.ElementToBeClickable());
+                    table_body = table.FindElement(By.TagName("tbody"));
+                    tds = table_body.FindElements(By.TagName("td"));
+
+                    foreach (IWebElement entry in tds)
+                    {
+                        attr = entry.GetAttribute("id");
+                        if (attr != "")
+                        {
+                            driver.Wait(50);
+                            entry.Click();
+                            foundAvailableDate = true;
+                            break;
+                        }
+                    }
+
+                    if (!foundAvailableDate)
+                    {
+                        nextmonth = driver.CurrentDriver.FindElement(By.ClassName("ui-datepicker-next"));
+                        nextmonth.Click();
+                    }
+            }
+            }
+            catch (NoSuchElementException nseEx)
+            {
+                Console.WriteLine(nseEx.Message);
+            }
+            catch (System.NullReferenceException nrfEx)
+            {
+                Console.WriteLine(nrfEx.Message);
+            }/*
+            catch (StaleElementReferenceException sterEx)
+            {
+                Console.WriteLine(sterEx.Message);
+            }*/
+        }
     }
 }
--- web_automation/WebDriver.cs
+++ web_automation/WebDriver.cs
@@ -18,6 +18,8 @@ using OpenQA.Selenium.Chrome;
 namespace web_automation
 {
     public enum IBrowser { CHROME, FIREFOX, EDGE}
+    public enum Element_By {ID, NAME, CLASSNAME,TAGNAME , XPATH}
+
 
     public class WebDriver
     {
@@ -38,17 +40,6 @@ namespace web_automation
             }
         }
 
-        private void withChrome(string path)
-        {
-            try
-            {
-                webdriver = new ChromeDriver(path);
-            } catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
         public void Close()
         {
             webdriver.Close();
@@ -63,6 +54,90 @@ namespace web_automation
         public IWebDriver CurrentDriver {
             get {
                 return webdriver;
+            }
+        }
+        
+        public IWebElement FindElement(string value, Element_By elem_by)
+        {
+            IWebElement element = null;
+            try
+            {
+                switch (elem_by)
+                {
+                    case Element_By.ID:
+                        element = webdriver.FindElement(By.Id(value));
+                        break;
+                    case Element_By.NAME:
+                        element = webdriver.FindElement(By.Name(value));
+                        break;
+                    case Element_By.CLASSNAME:
+                        element = webdriver.FindElement(By.ClassName(value));
+                        break;
+                    case Element_By.TAGNAME:
+                        element = webdriver.FindElement(By.TagName(value));
+                        break;
+                    case Element_By.XPATH:
+                        element = webdriver.FindElement(By.XPath(value));
+                        break;
+                }
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return element;
+        }
+
+        public IReadOnlyCollection<IWebElement> FindElements(string value, Element_By elem_by)
+        {
+            IReadOnlyCollection<IWebElement> elementsList = null;
+            try
+            {
+                switch (elem_by)
+                {
+                    case Element_By.ID:
+                        elementsList = webdriver.FindElements(By.Id(value));
+                        break;
+                    case Element_By.NAME:
+                        elementsList = webdriver.FindElements(By.Name(value));
+                        break;
+                    case Element_By.CLASSNAME:
+                        elementsList = webdriver.FindElements(By.ClassName(value));
+                        break;
+                    case Element_By.TAGNAME:
+                        elementsList = webdriver.FindElements(By.TagName(value));
+                        break;
+                    case Element_By.XPATH:
+                        elementsList = webdriver.FindElements(By.XPath(value));
+                        break;
+                }
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return elementsList;
+        }
+
+        public void Wait(uint sec)
+        {
+            webdriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(sec);
+        }
+
+        public void NavigateTo(string url)
+        {
+            webdriver.Navigate().GoToUrl(url);
+        }
+
+        private void withChrome(string path)
+        {
+            try
+            {
+                webdriver = new ChromeDriver(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
