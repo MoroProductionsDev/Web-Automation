6612bfbdc86a25d87a02eed03cb0bb4c7339aebc
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-12-31 11:24:28 -0800

    - Code Refactor - Commented - Mostly Functional. Except for a few parameters such as   skip-days, radio btn

--- web_automation/Program.cs
+++ web_automation/Program.cs
@@ -1,7 +1,8 @@
 ﻿/*# @author: Raul Rivero Rubio
 ### @date:  12/27/2019
 ### @version: 1.0
-### @brief: 
+### @brief: This application automate the process of filling a flighting form for Allegiant Airlines.
+###         Base on the options provide it will calculate the total mount of the flight.
 */
 
 using System;
@@ -20,15 +21,19 @@ namespace web_automation
 {
     class Program
     {
-        const string RELATIVE_PATH = "C:\\Users\\Jesus\\Documents\\C#\\alliante_automation\\web_automation\\web_automation\\drivers\\chromedriver_win32";
+        enum InputAcition {CLICK, SENDKEYS };
+
+        // Driver Local Location. Absolute Path Not a Resource
+        const string ABSOLUTE_PATH = "C:\\Users\\Jesus\\Documents\\C#\\alliante_automation\\web_automation\\web_automation\\drivers\\chromedriver_win32";
         const string HOMEPAGE = "https://www.allegiantair.com";
 
 
         static void Main(string[] args)
         {
-            web_automation.WebDriver driver = new WebDriver(RELATIVE_PATH, IBrowser.CHROME);
+            // Initiate Web Driver
+            web_automation.WebDriver driver = new WebDriver(ABSOLUTE_PATH, IBrowser.CHROME);
 
-            if (driver.CurrentDriver != null)
+            if (driver.isConEstablished())
             {
                 GoToHomePage(driver);
                 ClosePopUpWindow(driver);
@@ -36,165 +41,205 @@ namespace web_automation
             }
         }
 
+        /// <summary>
+        /// Navigates to website homepage.
+        /// </summary>
+        /// <param name="driver">The current drive use in the this section.</param>
         static void GoToHomePage(web_automation.WebDriver driver)
         {
             driver.NavigateTo(HOMEPAGE);
         }
 
+        /// <summary>
+        /// Closes a popup modal window.
+        /// </summary>
+        /// <param name="driver">The current drive use in the this section.</param>
         static void ClosePopUpWindow(web_automation.WebDriver driver)
         {
             IWebElement modalPopup;
             if ((modalPopup = driver.FindElement("ui-icon-closethick", Element_By.CLASSNAME)) != null)
             {
+                Thread.Sleep(1000);
                 modalPopup.Click();
             }
         }
 
-
+        /// <summary>
+        /// Proceeds with the automation on the landing page to book the trip.
+        /// </summary>
+        /// <param name="driver">The current drive use in the this section.</param>
         static void BookTrip(web_automation.WebDriver driver)
         {
-            IWebElement form_input;
-            IReadOnlyCollection<IWebElement> elemlist;
+            // Book Trip Options
             const string DEPARTURE_CITY = "Las Vegas, NV (LAS)";
             const string DESTINATION_CITY = "Albuquerque, NM (ABQ)";
+            // Const int (Enums)
             const int ROUND_TRIP = 0;
             const int ONEWAY = 1;
-            int choice = 1;
+            int trip_choice = 0;
+
+            Thread.Sleep(700);
+            // DEPARTURE_CITY
+            ActionOnInput(driver, Element_By.NAME, InputAcition.CLICK, "search_form[departure_city]", "");
+            ActionOnInput(driver, Element_By.NAME, InputAcition.SENDKEYS, "search_form[departure_city]", DEPARTURE_CITY);
+
+
+            Thread.Sleep(700);
+            // DESTINATION_CITY
+            try
+            {
+                // Same distination not allowed.
+                if (DESTINATION_CITY != DEPARTURE_CITY)
+                {
+                    ActionOnInput(driver, Element_By.NAME, InputAcition.CLICK, "search_form[destination_city]", "");
+                    ActionOnInput(driver, Element_By.NAME, InputAcition.SENDKEYS, "search_form[destination_city]", DESTINATION_CITY);
+                }
+                else
+                {
+                    throw new System.ArgumentException($"Departure city and destination city most be different. {DEPARTURE_CITY} : {DESTINATION_CITY}");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Thread.Sleep(1000);
-            if ((form_input = driver.FindElement("search_form[departure_city]", Element_By.NAME)) != null)
+            // Trip type (return) || (oneway)
+            if (trip_choice == ROUND_TRIP)
+            {
+                // NEEDS WORK
+                //driver.FindElement("//input[@type='radio'][@value='return']", Element_By.XPATH).Click();
+
+                // departure date
+                NextAvailablesDate(driver, "search_form[departure_date]", "depart", 0);
+
+                // return date
+                NextAvailablesDate(driver, "search_form[return_date]", "return", 0);
+            }
+            else if (trip_choice == ONEWAY)
+            {
+                driver.Wait(100);
+                // NEEDS WORK
+                //driver.FindElement("//input[@type='radio'][@value='oneway']", Element_By.XPATH).Click();
+
+                // departure date
+                NextAvailablesDate(driver, "search_form[departure_date]", "depart", 0);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="driver">The current drive use in the this section.</param>
+        /// <param name="by">The element method how the its going to be found.</param>
+        /// <param name="action">The action about to be perform on the element (clickable, typeable...)</param>
+        /// <param name="attribute_val">The attribute value that is use to find the element</param>
+        /// <param name="key_val">The text stream to be inserted in the input element.</param>
+        private static void ActionOnInput(web_automation.WebDriver driver, Element_By by, InputAcition action, string attribute_val, string keyboard_text)
+        {
+            IWebElement form_input;
+            if ((form_input = driver.FindElement(attribute_val, by)) != null)
             {
                 try
                 {
-                    form_input.Click();
-                    form_input.SendKeys(DEPARTURE_CITY);
+                    switch (action)
+                    {
+                        case InputAcition.CLICK: form_input.Click(); break;
+                        case InputAcition.SENDKEYS: form_input.SendKeys(keyboard_text); break;
+                    }
                 }
                 catch (ElementClickInterceptedException elmClkInt_Ex)
                 {
                     Console.WriteLine(elmClkInt_Ex.Message);
-                }
-            }
-
-            if ((form_input = driver.FindElement("search_form[destination_city]", Element_By.NAME)) != null)
-            {
-                form_input.Click();
-                try
+                } 
+                catch (ElementNotVisibleException elemNotVis_EX)
                 {
-                    if (DESTINATION_CITY != DEPARTURE_CITY)
-                    {
-                        form_input.SendKeys(DESTINATION_CITY);
-                    }
-                    else
-                    {
-                        throw new System.ArgumentException($"Departure city and destination city most be different. {DEPARTURE_CITY} : {DESTINATION_CITY}");
-                    }
+                    Console.WriteLine(elemNotVis_EX.Message);
                 }
-                catch (ArgumentException ex)
+                catch (InvalidElementStateException invElemState_Ex)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(invElemState_Ex.Message);
                 }
-            }
-
-            elemlist = driver.FindElements("search_form[trip_type]", Element_By.NAME);
-            Console.WriteLine(elemlist);
-            if (choice == ROUND_TRIP)
-            {
-                driver.FindElement("//input[@type='radio'][@value='return']", Element_By.XPATH).Click();
-
-                // departure date
-                NextAvailablesDate(driver, "search_form[departure_date]", "depart");
-
-                // return date
-                NextAvailablesDate(driver, "search_form[return_date]", "return");
-            }
-            else if (choice == ONEWAY)
-            {
-                driver.Wait(100);
-                driver.FindElement("//input[@type='radio'][@value='return']", Element_By.XPATH).Click();
-                driver.FindElement("//input[@type='radio'][@value='oneway']", Element_By.XPATH).Click();
-
-                // departure date
-                NextAvailablesDate(driver, "search_form[departure_date]", "depart");
+                catch (StaleElementReferenceException staElemRef_Ex)
+                {
+                    Console.WriteLine(staElemRef_Ex.Message);
+                }
             }
         }
 
-        private static void NextAvailablesDate(web_automation.WebDriver driver, string value, string at)
+        /// <summary>
+        /// Find the next Available date for departure or return.
+        /// </summary>
+        /// <param name="driver">The current drive use in the this section.</param>
+        /// <param name="attribute_val">The attribute value that is use to find the element.</param>
+        /// <param name="flightType">The type of flight either (oneway) || roundtrip (return).</param>
+        /// <param name="skip_days">The counter of how many available dates should be skip.</param>
+        private static void NextAvailablesDate(web_automation.WebDriver driver, string attribute_val, string flightType, uint skip_days)
         {
-            bool foundAvailableDate = false;
-            string attr;
-            IWebElement datepicker_div = null;
-            IWebElement table_head = null;
+            bool foundAvailableDate = false;            // flag for available date
+            string attr;                                // value of a attribute
             IWebElement table_body = null;
-            IWebElement form_input = null;
             IWebElement table = null;
-            IWebElement nextmonth = null;
-            IReadOnlyCollection<IWebElement> rows = null;
-            IReadOnlyCollection<IWebElement> tds = null;
-            WebDriverWait wait = new WebDriverWait(driver.CurrentDriver, TimeSpan.FromSeconds(20));
+            IWebElement nextmonth = null;               // ancher to table for the next month
+            IReadOnlyCollection<IWebElement> tds = null;    // all the cell in the table body
 
-            Actions actions = new Actions(driver.CurrentDriver);
-            if ((form_input = driver.FindElement(value, Element_By.NAME)) != null)
-            {
-                try
-                {
-                    form_input.Click();
-                } catch (ElementClickInterceptedException elemClckIntEX)
-                {
-                    Console.WriteLine(elemClckIntEX);
-                }
-            }
+
+            // Click
+            ActionOnInput(driver, Element_By.NAME, InputAcition.CLICK, attribute_val, "");
 
             Thread.Sleep(1000);
-            form_input.Click();
+
+            // Click
+            ActionOnInput(driver, Element_By.NAME, InputAcition.CLICK, attribute_val, "");
             try
             {
-                // 
-                datepicker_div = driver.FindElement(at, Element_By.CLASSNAME);
-                // actions.DoubleClick(calender).Perform();
-                //form_input.FindElement(By.ClassName("datepicker-toggle")).Click();
-                //table = datepicker_div.FindElement(By.ClassName("table"));
-
-                //table_body = datepicker_div.FindElement(By.TagName("tbody
-
-                //rows = table_body.FindElements(By.TagName("tr"));
-
-                //foreach (IWebElement row in rows)
-                //{
-                datepicker_div = driver.FindElement(at, Element_By.CLASSNAME);
-
+                // Keep Iterating until you find an available date
                 while (!foundAvailableDate)
                 {
-                    if (at == "depart")
+                    if (flightType == "depart")
                     {
-                        table = driver.CurrentDriver.FindElement(By.XPath("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[1]/div/div/div/div[2]/table"));
+                        // full (absolute) xpath
+                        table = driver.FindElement("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[1]/div/div/div/div[2]/table", Element_By.XPATH);
                     }
                     else
                     {
-                        table = driver.CurrentDriver.FindElement(By.XPath("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/table"));
+                        table = driver.FindElement("/html/body/div[5]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div[1]/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/table", Element_By.XPATH);
                     }
 
-                    //wait.Until(ExpectedConditions.ElementToBeClickable());
                     table_body = table.FindElement(By.TagName("tbody"));
                     tds = table_body.FindElements(By.TagName("td"));
 
+                    // Iterate through all the table cell
                     foreach (IWebElement entry in tds)
                     {
+                        // Only the available dates have an id.
                         attr = entry.GetAttribute("id");
+
+                        // if it is not empty
                         if (attr != "")
                         {
-                            driver.Wait(50);
-                            entry.Click();
-                            foundAvailableDate = true;
+                            // Check if their days needed to be skipped
+                            if (skip_days == 0)
+                            {
+                                driver.Wait(50);
+                                entry.Click();
+                                foundAvailableDate = true;
+                            } else
+                            {
+                                // is (Unsigned int). Only decrement while is not 0.
+                                --skip_days;
+                            }
                             break;
                         }
                     }
 
+                    // If no available date found in the month. Proceed to next month
                     if (!foundAvailableDate)
                     {
-                        nextmonth = driver.CurrentDriver.FindElement(By.ClassName("ui-datepicker-next"));
-                        nextmonth.Click();
+                        ActionOnInput(driver, Element_By.CLASSNAME, InputAcition.CLICK, "ui-datepicker-next", "");
                     }
-            }
+                }
             }
             catch (NoSuchElementException nseEx)
             {
@@ -203,11 +248,15 @@ namespace web_automation
             catch (System.NullReferenceException nrfEx)
             {
                 Console.WriteLine(nrfEx.Message);
-            }/*
-            catch (StaleElementReferenceException sterEx)
+            }
+            catch (ElementNotVisibleException elemNotVis_EX)
             {
-                Console.WriteLine(sterEx.Message);
-            }*/
+                Console.WriteLine(elemNotVis_EX.Message);
+            }
+            catch (StaleElementReferenceException staElemRef_Ex)
+            {
+                Console.WriteLine(staElemRef_Ex.Message);
+            }
         }
     }
 }
--- web_automation/WebDriver.cs
+++ web_automation/WebDriver.cs
@@ -24,12 +24,26 @@ namespace web_automation
     public class WebDriver
     {
         private IWebDriver webdriver = null;
-
+        /***
+        Parameterized Constructor
+        @brief  Attempts to connect the driver from the console to
+                    the web browser.
+        @param      path       the directory and file name (+extension) where
+                    the driver is located.
+        @param      browser     a enum with the desire browser to be connected.
+        */
         public WebDriver(string path, IBrowser browser)
         {
             Establish(path, browser);
         }
 
+        /***
+        @brief  Attempts to connect the driver from the console to
+                    the web browser.
+        @param      path       the directory and file name (+extension) where
+                    the driver is located.
+        @param      browser     a enum with the desire browser to be connected.
+        */    
         public void Establish(string path, IBrowser browser)
         {
             switch(browser)
@@ -39,24 +53,40 @@ namespace web_automation
                 case IBrowser.EDGE:; break;
             }
         }
-
+        /// <summary>
+        /// @brief    Close the web driver connection.
+        /// /// </summary>
         public void Close()
         {
             webdriver.Close();
             webdriver = null;
         }
 
+        /// <summary>
+        /// @brief    Checks if the driver connection was establish.
+        /// </summary>
+        /// <returns>If null (Con fail) | If not null (con success).</returns>
         public bool isConEstablished()
         {
             return webdriver != null;
         }
 
+        /// <summary>
+        ///  @brief    Checks whether a connection is established or not.
+        /// </summary>
         public IWebDriver CurrentDriver {
             get {
                 return webdriver;
             }
         }
-        
+
+        /// <summary>
+        /// Creates and return a web element found by the driver base on the value 
+        ///         and the method specify.
+        /// </summary>
+        /// <param name="value"> The value use to locate the element identifier</param>
+        /// <param name="elem_by"> The method use to find the element.</param>
+        /// <returns>A web element</returns>
         public IWebElement FindElement(string value, Element_By elem_by)
         {
             IWebElement element = null;
@@ -88,6 +118,13 @@ namespace web_automation
             return element;
         }
 
+        /// <summary>
+        /// Creates and return a readonly collection of web elements found by the driver base on the value 
+        ///         and the method specify.
+        /// </summary>
+        /// <param name="value"> The value use to locate the element identifier</param>
+        /// <param name="elem_by"> The method use to find the element.</param>
+        /// <returns>A read only collection of web elements</returns>
         public IReadOnlyCollection<IWebElement> FindElements(string value, Element_By elem_by)
         {
             IReadOnlyCollection<IWebElement> elementsList = null;
@@ -119,16 +156,36 @@ namespace web_automation
             return elementsList;
         }
 
+        /// <summary>
+        /// @brief      It delay the driver action depending on the time unit.
+        /// 
+        /// @param      sec     Time to be delay in seconds.
+        /// </summary>
+        /// <param name="sec"></param>
+
         public void Wait(uint sec)
         {
             webdriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(sec);
         }
 
+        /// <summary>
+        /// @brief      Navigate to specific url address.
+        /// 
+        /// @param      url     The Uniform Resource Locator.
+        /// </summary>
+        /// <param name="url"></param>
         public void NavigateTo(string url)
         {
             webdriver.Navigate().GoToUrl(url);
         }
 
+
+        /// <summary>
+        /// @brief    Attempts to connect with the Chrome browser.
+        /// @param    path       the directory and file name (+extension) where
+        /// the driver is located.
+        /// @throw    Exception if any error occurred establishing the connection.
+        /// </summary>
         private void withChrome(string path)
         {
             try
