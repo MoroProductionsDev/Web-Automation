9bbe804171a52951cb16e37afd8ee4b40c833343
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2020-01-01 14:08:37 -0800

    STABLE Version .Net Core. Rebuild and files imported. Dependicies issues Fixed.

--- web_automation/Properties/Resources.Designer.cs
+++ web_automation/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace web_automation.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] chromedriver {
-            get {
-                object obj = ResourceManager.GetObject("chromedriver", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
